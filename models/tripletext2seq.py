df53f7c61be6375e57fa056e7e681f816bb1fc84
shinochin <kazutoshi.shinoda0516@gmail.com>
2020-03-23 23:43:48 +0900

    fix

--- models/tripletext2seq.py
+++ models/tripletext2seq.py
@@ -107,7 +107,7 @@ class TripleText2SeqModel():
 
     def __build_single_rnn_cell(self, hidden_size):
 
-        cell = tf.keras.layers.GRUCell(hidden_size)
+        cell = tf.compat.v1.nn.rnn_cell.GRUCell(hidden_size)
 
         return cell
 
