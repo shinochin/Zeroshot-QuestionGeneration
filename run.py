e4f969ea6b3e32c7e5c757d8c80da27a718448e5
shinochin <kazutoshi.shinoda0516@gmail.com>
2020-03-30 02:39:59 +0900

    edit run.py

--- run.py
+++ run.py
@@ -29,7 +29,8 @@ parser.add_argument('-remove_unk', '--remove_unk', action='store_true')  # if me
 parser.add_argument('-o', '--out', help='output file name', required=False)
 parser.add_argument('-log', '--logfile', help='file to log outputs of experiment with params', required=False)
 
-
+parser.add_argument('-mode', '--mode', help='train, test or predict', required=True)
+parser.add_argument('-kb', '--kb', help='name of knowledge base', required=True)
 
 
 args = parser.parse_args()
@@ -38,6 +39,8 @@ args = parser.parse_args()
 allowed_exp_types = ["zeroshot", "normal", None]
 exp_type = None
 assert args.setup in allowed_exp_types
+assert args.mode in ['train', 'test']
+assert args.kb in ['freebase', 'wikidata']
 
 if args.setup is None:
     exp_type = "normal"
@@ -104,11 +107,13 @@ if exp_type == "normal":
 
     config.NUMBER_OF_TEXTUAL_EVIDENCES = text_n
 
-    traindatafeed = data.datafeed("train", config)
-    testdatafeed = data.datafeed("test", config)
+    if args.mode == 'train':
+        traindatafeed = data.datafeed("train", config)
+    if args.mode == 'test':
+        testdatafeed = data.datafeed("test", config)
 
 elif exp_type == "zeroshot":
-    data = ZeroShotsDataFeeder(datapath=args.datapath, remove_unk=args.remove_unk)
+    data = ZeroShotsDataFeeder(datapath=args.datapath, remove_unk=args.remove_unk, kb=args.kb)
     config = Config(data, model_name)
 
     if args.epochs is not None:
@@ -116,8 +121,10 @@ elif exp_type == "zeroshot":
 
     config.NUMBER_OF_TEXTUAL_EVIDENCES = text_n
 
-    traindatafeed = data.datafeed("train", config, args.criteria, kfold=kfolds, cv=fold, min_count=int(args.mincount))
-    testdatafeed = data.datafeed("test", config, args.criteria, kfold=kfolds, cv=fold, min_count=int(args.mincount))
+    if args.mode == 'train':
+        traindatafeed = data.datafeed("train", config, args.criteria, kfold=kfolds, cv=fold, min_count=int(args.mincount))
+    if args.mode == 'test':
+        testdatafeed = data.datafeed("test", config, args.criteria, kfold=kfolds, cv=fold, min_count=int(args.mincount))
 
 
 if args.out is None:
@@ -130,95 +137,28 @@ if args.logfile is None:
 #  START TRAINING #
 ###################
 
-if isbaseline:
-    model = Triple2SeqModel(config)
-else:
-    model = TripleText2SeqModel(config)
+if args.mode == 'train':
 
-print("Start Training model %s " % model_name)
-print("\n-----------------------\n")
+    if isbaseline:
+        model = Triple2SeqModel(config)
+    else:
+        model = TripleText2SeqModel(config)
 
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
+    print("Start Training model %s " % model_name)
+    print("\n-----------------------\n")
 
-    iterator = traindatafeed
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
 
-    for d in iterator:
-
-        encoder_triples_inputs, encoder_subtypes_inputs, encoder_subtypes_inputs_length, encoder_objtypes_inputs, encoder_objtypes_inputs_length, encoder_dep_inputs, encoder_dep_length, decoder_inputs, decoder_inputs_lengths, direction, meta = d
-
-        if isbaseline:
-
-            loss = model.train(sess, encoder_triples_inputs, decoder_inputs, decoder_inputs_lengths, direction)
-        else:
-
-            encoder_text_inputs = []
-            encoder_text_inputs_length = []
-            if args.subtype:
-                encoder_text_inputs.append(encoder_subtypes_inputs)
-                encoder_text_inputs_length.append(encoder_subtypes_inputs_length)
-            if args.objtype:
-                encoder_text_inputs.append(encoder_objtypes_inputs)
-                encoder_text_inputs_length.append(encoder_objtypes_inputs_length)
-            if args.pred:
-                encoder_text_inputs.append(encoder_dep_inputs)
-                encoder_text_inputs_length.append(encoder_dep_length)
-
-            loss = model.train(sess, encoder_triples_inputs, encoder_text_inputs, encoder_text_inputs_length, decoder_inputs, decoder_inputs_lengths, direction)
-
-        if model.global_step.eval() % config.LOG_FREQUENCY == 0:
-            print("Global Step %s Epoch %s  Batch %s \t Loss = %s" % (model.global_step.eval(), meta["epoch"], meta["batch_id"], np.mean(loss)))
-
-        # Save the model checkpoint
-        if model.global_step.eval() % config.SAVE_FREQUENCY == 0:
-
-            print('Saving the model..')
-            checkpoint_path = os.path.join(config.CHECKPOINTS_PATH)
-            path = model.save(sess, checkpoint_path, global_step=model.global_step)
-
-###########
-# testing #
-###########
-
-tf.reset_default_graph()
-results = []
-
-if isbaseline:
-    model = Triple2SeqModel(config, 'inference')
-else:
-    model = TripleText2SeqModel(config, 'inference')
-
-predicted_ids = []
-
-
-def post_process(s, d):
-
-    d = sorted(d, key=lambda i: len(i[1]))
-
-    for v, k in d:
-        s = s.replace(k, v)
-
-    return s
-
-with tf.Session() as sess:
-
-    if tf.train.checkpoint_exists(tf.train.latest_checkpoint(os.path.dirname(config.CHECKPOINTS_PATH))):
-        print('reloading the trained model')
-
-        model.restore(sess=sess, path=tf.train.latest_checkpoint(os.path.dirname(config.CHECKPOINTS_PATH)))
-
-        predicted = []
-        labels = []
-
-        iterator = testdatafeed
+        iterator = traindatafeed
 
         for d in iterator:
 
             encoder_triples_inputs, encoder_subtypes_inputs, encoder_subtypes_inputs_length, encoder_objtypes_inputs, encoder_objtypes_inputs_length, encoder_dep_inputs, encoder_dep_length, decoder_inputs, decoder_inputs_lengths, direction, meta = d
 
             if isbaseline:
-                predicted_ids = model.predict(sess, encoder_triples_inputs=encoder_triples_inputs,
-                                              encoder_predicates_direction=direction)
+
+                loss = model.train(sess, encoder_triples_inputs, decoder_inputs, decoder_inputs_lengths, direction)
             else:
 
                 encoder_text_inputs = []
@@ -233,65 +173,136 @@ with tf.Session() as sess:
                     encoder_text_inputs.append(encoder_dep_inputs)
                     encoder_text_inputs_length.append(encoder_dep_length)
 
-                predicted_ids = model.predict(sess, encoder_triples_inputs=encoder_triples_inputs,
-                                              encoder_text_inputs=encoder_text_inputs,
-                                              encoder_text_inputs_length=encoder_text_inputs_length,
-                                              encoder_predicates_direction=direction)
+                loss = model.train(sess, encoder_triples_inputs, encoder_text_inputs, encoder_text_inputs_length, decoder_inputs, decoder_inputs_lengths, direction)
 
-            for c, i in enumerate(encoder_triples_inputs):
+            if model.global_step.eval() % config.LOG_FREQUENCY == 0:
+                print("Global Step %s Epoch %s  Batch %s \t Loss = %s" % (model.global_step.eval(), meta["epoch"], meta["batch_id"], np.mean(loss)))
 
-                sub = data.inv_entityvocab[i[0]]
-                pred = data.inv_propertyvocab[i[1]]
-                obj = data.inv_entityvocab[i[2]]
+            # Save the model checkpoint
+            if model.global_step.eval() % config.SAVE_FREQUENCY == 0:
 
-                y = " ".join([data.inv_wordvocab[i] for i in np.squeeze(predicted_ids[c], axis=1)])
-                y_label = " ".join([data.inv_wordvocab[i] for i in decoder_inputs[c]])
+                print('Saving the model..')
+                checkpoint_path = os.path.join(config.CHECKPOINTS_PATH)
+                path = model.save(sess, checkpoint_path, global_step=model.global_step)
 
-                y_post_proc = post_process(y, meta["placeholder_dict"][c])
-                y_label_post_proc = post_process(y_label, meta["placeholder_dict"][c])
+###########
+# testing #
+###########
 
-                subtype = " ".join([data.inv_wordvocab[i] for i in encoder_subtypes_inputs[c]])
-                objtype = " ".join([data.inv_wordvocab[i] for i in encoder_objtypes_inputs[c]])
-                dep = " ".join([data.inv_wordvocab[i] for i in encoder_dep_inputs[c]])
+if args.mode == 'test':
 
-                results.append([sub, pred, obj, subtype, objtype, dep, y, y_label, y_post_proc, y_label_post_proc])
+    tf.reset_default_graph()
+    results = []
 
-                predicted.append(y)
-                labels.append(y_label)
+    if isbaseline:
+        model = Triple2SeqModel(config, 'inference')
+    else:
+        model = TripleText2SeqModel(config, 'inference')
 
-        results_df = pd.DataFrame(results, columns=["sub", "pred", "obj", "subtype", "objtype", "dep", "y", "y_label", "y_post_proc", "y_label_post_proc"])
-        results_df.to_csv(args.out, encoding="utf-8")
+    predicted_ids = []
 
-        # remove start
-        predicted = [" ".join(i.split()[:-1]).encode('ascii', 'ignore') for i in predicted]
-        labels = [" ".join(i.split()[1:-1]).encode('ascii', 'ignore') for i in labels]
-        e = Evaluator(predicted, labels)
-        e.evaluate()
 
-        s = list()
-        s.append(("model_Name", model_name))
-        s.append(("DECODER_RNN_HIDDEN_SIZE", config.DECODER_RNN_HIDDEN_SIZE))
+    def post_process(s, d):
 
-        s.append(("COUPLE_ENCODER_DECODER_WORD_EMBEDDINGS", config.COUPLE_ENCODER_DECODER_WORD_EMBEDDINGS))
-        if config.USE_PRETRAINED_WORD_EMBEDDINGS:
-            s.append(("USE_PRETRAINED_WORD_EMBEDDINGS", config.PRETRAINED_WORD_EMBEDDINGS_PATH))
-            s.append(("TRAIN_WORD_EMBEDDINGS", config.TRAIN_WORD_EMBEDDINGS))
-        else:
-            s.append(("USE_PRETRAINED_WORD_EMBEDDINGS", "FALSE"))
+        d = sorted(d, key=lambda i: len(i[1]))
 
-        s.append(("USE_PRETRAINED_KB_EMBEDDINGS", config.USE_PRETRAINED_KB_EMBEDDINGS))
-        s.append(("TRAIN_KB_EMBEDDINGS", config.TRAIN_KB_EMBEDDINGS))
+        for v, k in d:
+            s = s.replace(k, v)
 
-        s.append(("Batch Size", config.BATCH_SIZE))
-        s.append(("Epochs", config.MAX_EPOCHS))
-        s.append(("LR", config.LR))
-        s.append(("MAX_GRAD_NORM", config.MAX_GRAD_NORM))
+        return s
 
-        for i in e.overall_eval.items():
-            s.append(i)
+    with tf.Session() as sess:
 
-        with open(args.logfile, 'a+') as f:
-            f.write("\n------------\n")
-            for k in s:
-                print("%s \t %s" % (k[0], k[1]))
-                f.write("%s \t %s\n" % (k[0], k[1]))
+        if tf.train.checkpoint_exists(tf.train.latest_checkpoint(os.path.dirname(config.CHECKPOINTS_PATH))):
+            print('reloading the trained model')
+
+            model.restore(sess=sess, path=tf.train.latest_checkpoint(os.path.dirname(config.CHECKPOINTS_PATH)))
+
+            predicted = []
+            labels = []
+
+            iterator = testdatafeed
+
+            for d in iterator:
+
+                encoder_triples_inputs, encoder_subtypes_inputs, encoder_subtypes_inputs_length, encoder_objtypes_inputs, encoder_objtypes_inputs_length, encoder_dep_inputs, encoder_dep_length, decoder_inputs, decoder_inputs_lengths, direction, meta = d
+
+                if isbaseline:
+                    predicted_ids = model.predict(sess, encoder_triples_inputs=encoder_triples_inputs,
+                                                  encoder_predicates_direction=direction)
+                else:
+
+                    encoder_text_inputs = []
+                    encoder_text_inputs_length = []
+                    if args.subtype:
+                        encoder_text_inputs.append(encoder_subtypes_inputs)
+                        encoder_text_inputs_length.append(encoder_subtypes_inputs_length)
+                    if args.objtype:
+                        encoder_text_inputs.append(encoder_objtypes_inputs)
+                        encoder_text_inputs_length.append(encoder_objtypes_inputs_length)
+                    if args.pred:
+                        encoder_text_inputs.append(encoder_dep_inputs)
+                        encoder_text_inputs_length.append(encoder_dep_length)
+
+                    predicted_ids = model.predict(sess, encoder_triples_inputs=encoder_triples_inputs,
+                                                  encoder_text_inputs=encoder_text_inputs,
+                                                  encoder_text_inputs_length=encoder_text_inputs_length,
+                                                  encoder_predicates_direction=direction)
+
+                for c, i in enumerate(encoder_triples_inputs):
+
+                    sub = data.inv_entityvocab[i[0]]
+                    pred = data.inv_propertyvocab[i[1]]
+                    obj = data.inv_entityvocab[i[2]]
+
+                    y = " ".join([data.inv_wordvocab[i] for i in np.squeeze(predicted_ids[c], axis=1)])
+                    y_label = " ".join([data.inv_wordvocab[i] for i in decoder_inputs[c]])
+
+                    y_post_proc = post_process(y, meta["placeholder_dict"][c])
+                    y_label_post_proc = post_process(y_label, meta["placeholder_dict"][c])
+
+                    subtype = " ".join([data.inv_wordvocab[i] for i in encoder_subtypes_inputs[c]])
+                    objtype = " ".join([data.inv_wordvocab[i] for i in encoder_objtypes_inputs[c]])
+                    dep = " ".join([data.inv_wordvocab[i] for i in encoder_dep_inputs[c]])
+
+                    results.append([sub, pred, obj, subtype, objtype, dep, y, y_label, y_post_proc, y_label_post_proc])
+
+                    predicted.append(y)
+                    labels.append(y_label)
+
+            results_df = pd.DataFrame(results, columns=["sub", "pred", "obj", "subtype", "objtype", "dep", "y", "y_label", "y_post_proc", "y_label_post_proc"])
+            results_df.to_csv(args.out, encoding="utf-8")
+
+            # remove start
+            predicted = [" ".join(i.split()[:-1]).encode('ascii', 'ignore') for i in predicted]
+            labels = [" ".join(i.split()[1:-1]).encode('ascii', 'ignore') for i in labels]
+            e = Evaluator(predicted, labels)
+            e.evaluate()
+
+            s = list()
+            s.append(("model_Name", model_name))
+            s.append(("DECODER_RNN_HIDDEN_SIZE", config.DECODER_RNN_HIDDEN_SIZE))
+
+            s.append(("COUPLE_ENCODER_DECODER_WORD_EMBEDDINGS", config.COUPLE_ENCODER_DECODER_WORD_EMBEDDINGS))
+            if config.USE_PRETRAINED_WORD_EMBEDDINGS:
+                s.append(("USE_PRETRAINED_WORD_EMBEDDINGS", config.PRETRAINED_WORD_EMBEDDINGS_PATH))
+                s.append(("TRAIN_WORD_EMBEDDINGS", config.TRAIN_WORD_EMBEDDINGS))
+            else:
+                s.append(("USE_PRETRAINED_WORD_EMBEDDINGS", "FALSE"))
+
+            s.append(("USE_PRETRAINED_KB_EMBEDDINGS", config.USE_PRETRAINED_KB_EMBEDDINGS))
+            s.append(("TRAIN_KB_EMBEDDINGS", config.TRAIN_KB_EMBEDDINGS))
+
+            s.append(("Batch Size", config.BATCH_SIZE))
+            s.append(("Epochs", config.MAX_EPOCHS))
+            s.append(("LR", config.LR))
+            s.append(("MAX_GRAD_NORM", config.MAX_GRAD_NORM))
+
+            for i in e.overall_eval.items():
+                s.append(i)
+
+            with open(args.logfile, 'a+') as f:
+                f.write("\n------------\n")
+                for k in s:
+                    print("%s \t %s" % (k[0], k[1]))
+                    f.write("%s \t %s\n" % (k[0], k[1]))
